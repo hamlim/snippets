d2ea62a1c917fbb7f6b5944af43704b4b80e83e9
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-04-20 19:48:30 -0400

    native-form-back-button

---
+++ native-form-back-button.tsx
@@ -0,0 +1,12 @@
+<button
+  type="submit"
+  // handle this form submit in your form reducer
+  name="back"
+  // can be anything you want
+  value="true"
+  // allow form submissions even if there are required form fields
+  // not currently filled in!
+  formNoValidate
+>
+  Go Back
+</button>;
